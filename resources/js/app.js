f2326144ff6d856bd621e164f45989a20500885b
cochon123 <tchiosemepierrearthur@gmail.com>
2025-10-04 07:46:23 -0600

    fix: réorganiser l'importation des modules dans app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,15 +5,6 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-<<<<<<< HEAD
-
-import './bootstrap';
-
-import Alpine from 'alpinejs';
-
-window.Alpine = Alpine;
-
-Alpine.start();
 
 // PWA Install functionality
 let deferredPrompt;
